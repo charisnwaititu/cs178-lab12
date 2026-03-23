984bdb1e98d2830a6363971a59ecdd624586ad6a
charisnwaititu <charis.waititu@drake.edu>
2026-03-22 20:27:47 -0500

    Exercise 3

--- flaskapp.py
+++ flaskapp.py
@@ -37,22 +37,45 @@ def hello(name):
 
 # ---- Exercise 2: update your analyze route ----
 
+# @app.route('/analyze/<word>')
+# def analyze(word):
+#     # Step 1: character count (already done)
+#     num_chars = len(word)
+
+#     # Step 2: YOUR CODE HERE
+#     num_vowels = 0  
+#     for char in word.lower():
+#         if char in "aeiou":
+#             num_vowels += 1
+
+#     # render_template passes all variables into analyze.html
+#     return render_template('analyze.html',
+#                            word=word,
+#                            num_chars=num_chars,
+#                            num_vowels=num_vowels)
+
+
+# ---- Exercise 3: update your analyze route again ----
+
 @app.route('/analyze/<word>')
 def analyze(word):
-    # Step 1: character count (already done)
     num_chars = len(word)
 
-    # Step 2: YOUR CODE HERE
-    num_vowels = 0  
+    num_vowels = 0  # your vowel code from Exercise 2
     for char in word.lower():
-        if char in "aeiou":
+        if char in 'aeiou':
             num_vowels += 1
 
-    # render_template passes all variables into analyze.html
+    # YOUR CODE HERE
+    # Reverse the word
+    # Hint: Python string slicing with [::-1] reverses a string
+    reversed_word = word[::-1] # replace this with your reverse logic
+
     return render_template('analyze.html',
                            word=word,
                            num_chars=num_chars,
-                           num_vowels=num_vowels)
+                           num_vowels=num_vowels,
+                           reversed_word=reversed_word)
 
 
 # ============================================================
